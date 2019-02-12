044f8e797722a9481b14217bc413a975949f586d
KyleMac998 <45363919+KyleMac998@users.noreply.github.com>
2019-02-12 16:06:21 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- Fetching and processing data and using this with Google Maps
+     Data feed URLs can be found on following page: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+     Author: Peter Barrie
+     Date: 7 August 2018
+Specifically:
+     Click button to populate map with earthquake markers from a specific USGS data feed.
+     Markers are labelled to show the magnitude of the earthquake.
+-->
+<html>
+
+<head>
+  <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
+
+  meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">IP3</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">OverView</a></li>
+      <li class="active"><a href="EarthQuake.php">EarthQuake Map</a></li>
+    </ul>
+  </div>
+</nav>
+</head>
+
+<body>
+  
+
+</body>
+
+
+
+</html>
